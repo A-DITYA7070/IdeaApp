89ed7b0f5a7ddd1516a7df06cb5c4548eb91d59f
A-DITYA7070 <99244575+A-DITYA7070@users.noreply.github.com>
2023-08-03 12:15:57 +0530

    adding the config folder

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express=require('express');
-
+const config = require('./config/configs.js');
 const app=express();
 
-const PORT=3000
-app.listen(3000,()=>{
-    console.log(`server is running on port http//localhost:${PORT}`);
-})
+app.listen(config.PORT,()=>{
+    console.log(`server is running on port http://localhost:${config.PORT}`);
+});
